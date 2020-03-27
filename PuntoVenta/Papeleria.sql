bda663745d6de92b04c93c864d32ceb22074b3e7
IngSistemasOmarAguilar <62722584+IngSistemasOmarAguilar@users.noreply.github.com>
2020-03-27 12:42:48 -0600

    Actualizr base de datos

--- PuntoVenta/Papeleria.sql
+++ PuntoVenta/Papeleria.sql
@@ -1,2 +1,5 @@
 create database Papeleria;
 use Papeleria;
+create table Roleas(id_rol int primary key auto_increment , descripcion varchar(30));
+insert into Roleas(descripcion) values("Administrador");
+insert into Roleas(descripcion) values("Empleado");
